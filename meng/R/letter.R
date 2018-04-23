e2653792622b3aab38b1565610dbf807b00e39d7
mxie1029 <38534863+mxie1029@users.noreply.github.com>
2018-04-23 15:45:35 -0500

    Add files via upload

---
+++ meng/R/letter.R
@@ -0,0 +1,13 @@
+#' generating a letter
+#'
+#' sample from the alphabet
+#'
+#' @param x A useless parameter for the parallel computition
+#' @return a random letter of the alphabet
+#' @export
+
+letter <- function(x){
+ sample(letters,1)
+}
+
+
